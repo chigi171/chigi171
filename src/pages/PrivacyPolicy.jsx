00b1a8a6038cee2ecf293c91ba9d54d9701ef07b
chigi171 <bazariweb@gmail.com>
2024-12-24 23:32:05 +0000

    fix pages

---
+++ src/pages/PrivacyPolicy.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PrivacyPolicy = () => (
+    <div className="pt-20 pb-12">
+        <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold mb-8">Privacy Policy</h1>
+            <div className="prose lg:prose-lg">
+                <div class="ql-editor">test</div>
+            </div>
+        </div>
+    </div>
+);
+
+export default PrivacyPolicy;
